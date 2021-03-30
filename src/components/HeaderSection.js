9096aad0d2aa5d3867bbff1427870c2312b81476
Al-Shaikh-Ul-Habibi <mohammadaatif@protonmail.com>
2021-03-31 00:14:48 +0530

    Responsive ness in header section

--- src/components/HeaderSection.js
+++ src/components/HeaderSection.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import './HeaderSection.css';
 import '../App.css';
 import video1 from "../videos/video-1.mp4";
 import video2 from "../videos/video-2.mp4";
 
 function HeaderSection(){
+    const [video,setVideo] = useState(false);
+    const showVideo=()=>{
+        if(window.innerWidth <= 760){
+            setVideo(false);
+        }
+        else{
+            setVideo(true);
+        }
+    };
+    useEffect(()=>{
+        showVideo();
+    },[]);
+    
+    window.addEventListener('resize', showVideo);
     return(
         <div className="hero-container">
-            <video src={video2} autoPlay loop muted />
-            <h1> Lets be A Crazy Tourist</h1>
-            <p> Lets we plan your crazy tour</p>
-
+            <video src={video?video2:video1} autoPlay loop muted />
+            <h1>A Crazy Tourist</h1>
+            <p>Lets we plan your crazy tour</p>
         </div>
     );
 }
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,12 @@
 import '../../App.css';
 import HeaderSection from '../HeaderSection';
+import Cards from '../Cards'
 
 function Home() {
     return(
         <>
             <HeaderSection/>
+            <Cards/>
         </>
     )
 }
